2aef61347e62f99f2389a1abd3a1cd4e6827aba1
James Ferreira <James.D.Ferreira@pm.me>
2025-11-06 18:43:54 -0700

    Change Uvicorn port from 8000 to 8001

--- backend/main.py
+++ backend/main.py
@@ -73,7 +73,7 @@ def ping():
 
 # --- run locally with uvicorn when this file is the entrypoint ---
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
 
 # --- pip install requirements for MacOS ---
 # do this once before running anything else
